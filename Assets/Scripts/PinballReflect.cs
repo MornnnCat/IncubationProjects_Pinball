5e88254351d1177e512bd379eb23abe331a7862b
MornnnCat <99341958+MornnnCat@users.noreply.github.com>
2024-08-29 16:02:13 +0800

    test2

--- Assets/Scripts/PinballReflect.cs
+++ Assets/Scripts/PinballReflect.cs
@@ -19,7 +19,7 @@ public class PinballReflect : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log("3");
+        Debug.Log("5");
         moveDir = GameObject.Find("FirePoint").transform.up * pinballMoveSpeed;
     }
 
